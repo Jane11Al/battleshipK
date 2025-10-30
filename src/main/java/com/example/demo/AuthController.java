4808b44126331ea8edd4df9de31b484f3eecef6c
Nastya Rasskazova <nastaytix@gmail.com>
2025-10-30 14:28:44 +0400

    first

--- src/main/java/com/example/demo/AuthController.java
+++ src/main/java/com/example/demo/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 //gsjhfkl
+//привет
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
